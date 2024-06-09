c680513088a494916eded9c3d3bd982788700028
david <davidobinna579@gmail.com>
2024-06-09 13:32:16 +0100

    initial-commit

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,4 +6,21 @@ router.route("/").get((request, response) => {
 })
 
 
+router.route("/").post((request, response) => {
+    response.status(200).json({message: "Create contacts"})
+})
+
+router.route("/:id").get((request, response) => {
+    response.status(200).json({message: `Get contact for ID: ${request.params.id}` })
+})
+
+router.route("/:id").put((request, response) => {
+    response.status(200).json({message: `Update contact for ID: ${request.params.id}`})
+})
+
+router.route("/:id").delete((request, response) => {
+    response.status(200).json({message: `Deleted contact for ID: ${request.params.id}`})
+})
+
+
 module.exports = router;
