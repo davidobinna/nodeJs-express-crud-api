e966c2de973acc066a393e3e8914149feafe5eea
david <davidobinna579@gmail.com>
2024-06-09 13:52:23 +0100

    initial-commit

---
+++ controllers/contactController.js
@@ -0,0 +1,37 @@
+//@desc GET all contact
+//@route GET /api/contacts
+//@access public 
+
+const getContact = (request, response) => {
+    response.status(200).json({message: "Get all contacts"})
+};
+
+
+//@desc Create new contact
+//@route POST/api/contacts
+//@access public
+
+const createContact = (request, response) => {
+   response.status(201).json({message: "Create contacts"})
+}
+
+
+//@desc Get contact by Id
+//@route GET/api/contacts/:id
+//@access public
+
+const getContactById = (request, response) => {
+    response.status(200).json({message: `Get contact for ID: ${request.params.id}` })
+ }
+
+
+//@desc Update contact by Id
+//@route UPDATE/api/contacts/:id
+//@access public
+
+const updateContact = (request, response) => {
+    response.status(200).json({message: `Update contact for ID: ${request.params.id}`})
+ }
+
+
+module.exports = { getContact, createContact, getContactById, updateContact };
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,24 +1,18 @@
 const express = require("express")
 const router = express.Router();
-
-router.route("/").get((request, response) => {
-    response.status(200).json({message: "Get all contacts"})
-})
+const {getContact, createContact, getContactById, updateContact} = require("../controllers/contactController");
 
 
-router.route("/").post((request, response) => {
-    response.status(200).json({message: "Create contacts"})
-})
+router.route("/contacts").get(getContact);
 
-router.route("/:id").get((request, response) => {
-    response.status(200).json({message: `Get contact for ID: ${request.params.id}` })
-})
 
-router.route("/:id").put((request, response) => {
-    response.status(200).json({message: `Update contact for ID: ${request.params.id}`})
-})
+router.route("/contacts").post(createContact)
 
-router.route("/:id").delete((request, response) => {
+router.route("/contacts/:id").get(getContactById)
+
+router.route("/contacts/:id").put(updateContact)
+
+router.route("/contacts/:id").delete((request, response) => {
     response.status(200).json({message: `Deleted contact for ID: ${request.params.id}`})
 })
 
